d886e9c505c16f506317e2266e39a5b57ff36715
oliver.coulson <oliver.coulson@imanage.com>
2020-11-27 16:15:47 +0000

    Starting to remove nest stuff

--- src/backend/to-do/to-do.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ToDoResolver } from './to-do.resolver';
-import { ToDoService } from './to-do.service';
-
-@Module({
-  providers: [ToDoService, ToDoResolver],
-})
-export class ToDoModule {}
